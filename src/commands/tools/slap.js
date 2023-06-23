71195da61ca185e91d1da9b0e9dd324fda20c4de
Victo <vsingh57@myseneca.ca>
2023-06-23 16:11:48 -0400

    Added Django Rest framework

--- src/commands/tools/slap.js
+++ src/commands/tools/slap.js
@@ -4,10 +4,10 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('slap')
         .setDescription('Slap someone!')
-        .addUserOption(options => 
-                options.setName('slapee')
-                    .setDescription('choose your victim')
-                    .setRequired(true)),
+        .addUserOption(options =>
+            options.setName('slapee')
+                .setDescription('choose your victim')
+                .setRequired(true)),
     async execute(interaction, client) {
         await interaction.deferReply()
         client.emit('onSlap', interaction, client);
